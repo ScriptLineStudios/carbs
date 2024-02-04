42cd832745bbc483e64b42ae2618b85f22c95417
ScriptLineStudios <scriptlinestudios@protonmail.com>
2024-02-04 11:30:57 +0200

    yes

--- main.py
+++ main.py
@@ -35,11 +35,9 @@ def generate():
         return utils.error("Please include your code!")
 
     url = f"https://carbon.now.sh/?wc={str(window_controls).lower()}&wt={window_theme}&fm={font}&fs={font_size}px&l={lang}&t={theme}&pv={pv}px&ph={ph}px&es=2x&code={code}"
-    # image = utils.screenshot(url, driver)
     image = utils.screenshot(url)
-
     return send_file(image), 200
 
 if __name__ == "__main__":
-    app.run(host="", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=False)
 
